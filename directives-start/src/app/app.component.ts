a6821e8d57d8452fe070ec6cf906ad15869295fb
Rashpal Gill <rsgillit@gmail.com>
2019-08-06 11:56:49 -0500

    another one

--- directives-start/src/app/app.component.ts
+++ directives-start/src/app/app.component.ts
@@ -6,10 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  loadedFeature = 'recipe';
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-    console.log(this.loadedFeature);
-  }
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1 ,3, 5];
+  evenNumbers = [2, 4]
+  onlyOdd = false;
 }
--- recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,9 +8,10 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','this is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2019/07/stuffed-zucchini-recipe-1-2.jpg'),
-    new Recipe('A Test Recipe','this is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2019/07/stuffed-zucchini-recipe-1-2.jpg')
+    new Recipe('Another Test Recipe','this is simply a test','https://cdn.loveandlemons.com/wp-content/uploads/2019/07/stuffed-zucchini-recipe-1-2.jpg')
 
   ];
   
@@ -19,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
